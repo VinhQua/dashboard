2e050d31cc899764ab2ebd0381b80d08e5939df5
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-05-02 12:37:33 +0900

    contact

--- src/scenes/global/TopBar.jsx
+++ src/scenes/global/TopBar.jsx
@@ -8,7 +8,13 @@ import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import SearchIcon from '@mui/icons-material/Search';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import { Link } from "react-router-dom";
+
 const TopBar = () => {
+
     const theme = useTheme()
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(colorModeContext);
@@ -26,7 +32,28 @@ const TopBar = () => {
                     <SearchIcon/>
                 </IconButton>
             </Box>
+            {/* Contact Info */}
+            <Box display="flex" gap="2rem" >
+            <Link to="https://github.com/VinhQua" target="_blank" underline="none">
+                <IconButton>
+                  <GitHubIcon/>  
+                </IconButton>
+                
+            </Link>
 
+            <Link to="https://www.linkedin.com/in/vinhquafrontenddeveloper/" target="_blank" underline="none">
+                <IconButton>
+                   <LinkedInIcon/> 
+                </IconButton>
+                
+            </Link>
+
+            <Link to="mailto:vinhqua.io@gmail.com" target="_blank" underline="none">
+                <IconButton>
+                   <EmailOutlinedIcon/> 
+                </IconButton>
+            </Link>
+            </Box>
             {/* Icons */}
             <Box display="flex">
                 <IconButton onClick={colorMode.toggleColorMode}>
